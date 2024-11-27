68d94b46bb3e3952a2c20b4ba8248074d0d70813
K N Murali Royal <muraliyadav0208@gmail.com>
2024-11-27 17:55:22 +0530

    first commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+        <div class="self-start text-sm">Laracasts</div>
+        <div class="py-8 font-bold">
+            <h3>Video Producer</h3>
+            <p>Full time - from $60,000</p>
+        </div>
+        <div class="flex justify-between items-center mt-auto">
+            <div>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+
+            </div>
+            <img src="http://placehold.it/42/42" alt="">
+        </div>
+
+    </div>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <x-app-layout>
+        <div class="space-y-10">
+            <section>
+                <x-section-heading>Feature Jobs</x-section-heading>
+                <div class="grid lg:grid-cols-3 gap-8 mt-6">
+                    <x-job-card></x-job-card>
+                    <x-job-card></x-job-card>
+                    <x-job-card></x-job-card>
+                </div>
+            </section>
+            <section>
+                <x-section-heading>Tags</x-section-heading>
+
+            </section>
+            <section>
+                <x-section-heading>Recent Jobs</x-section-heading>
+                <div class="mt-6 space-x-1">
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                    <x-tag>Tag</x-tag>
+                </div>
+            </section>
+        </div>
+    </x-app-layout>
+</body>
+
+</html>
